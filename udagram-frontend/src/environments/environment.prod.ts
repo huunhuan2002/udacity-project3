9fc3a1477fbd671b74d3aa96ffd0fcae7de91cbe
Le Huu Nhuan <huunhuan2002@gmail.com>
2022-04-18 23:35:48 +0700

    update

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a3b48461d4f1745b6b8dd052a04df2bd-701852187.us-east-1.elb.amazonaws.com/:8080/api/v0'
+  apiHost: 'http://a3b48461d4f1745b6b8dd052a04df2bd-701852187.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
